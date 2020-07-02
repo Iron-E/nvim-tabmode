509c06312d5d03855b5a0b67ff40f867a1c55fe9
Iron_E <code.iron.e@gmail.com>
2020-07-02 12:20:32 -0400

    Fix attempting to string.char() a non-ascii character

--- lua/tabmode/init.lua
+++ lua/tabmode/init.lua
@@ -29,11 +29,13 @@ local _combos = {
 	}}) end,
 }
 
+local function to_char(val) return api.nvim_eval('"\\'..val..'"') end
+
 local _go_to_beginning = {'^', '0'}
-local _move_left       = {'b', 'j', 'h', "<Left>"}
-local _move_right      = {'w', 'k', 'l', "<Right>"}
-local _shift_left      = {'B', 'J', 'H', "<S-Left>"}
-local _shift_right     = {'W', 'K', 'L', "<S-Right>"}
+local _move_left       = {'b', 'j', 'h', to_char('<Left>')}
+local _move_right      = {'w', 'k', 'l', to_char('<Right>')}
+local _shift_left      = {'B', 'J', 'H', to_char('<S-Left>')}
+local _shift_right     = {'W', 'K', 'L', to_char('<S-Right>')}
 
 --[[
 	/*
@@ -42,15 +44,18 @@ local _shift_right     = {'W', 'K', 'L', "<S-Right>"}
 --]]
 
 local function _modeInstruction()
-	-- local uinput = string.char(vim.api.nvim_get_var('tabsModeInput'))
-	local uinput = string.char(api.nvim_get_var('tabsModeInput'))
+	-- Get the raw character value of the input.
+	local uinput = api.nvim_get_var('tabsModeInput')
+	-- If `getchar()` reported a number, convert it back to a character.
+	uinput = (type(uinput) == 'number') and string.char(uinput) or uinput
 
 	    if contains(_go_to_beginning , uinput) then nvim_command('tabfirst'   )
 	elseif contains(_move_left       , uinput) then nvim_command('tabprevious')
 	elseif contains(_shift_left      , uinput) then nvim_command('-tabmove'   )
 	elseif contains(_move_right      , uinput) then nvim_command('tabnext'    )
 	elseif contains(_shift_right     , uinput) then nvim_command('+tabmove'   )
-	elseif _combos[uinput]                      then _combos[uinput]()                end
+	elseif _combos[uinput]                     then _combos[uinput]()
+	end
 end
 
 --[[
