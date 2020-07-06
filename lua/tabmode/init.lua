260dce7b94ff30ff296dd346a18655afba60eb25
Iron_E <code.iron.e@gmail.com>
2020-07-06 11:37:09 -0400

    Add many more keys

--- lua/tabmode/init.lua
+++ lua/tabmode/init.lua
@@ -30,12 +30,17 @@ local _combos = {
 
 local function to_char(val) return api.nvim_eval('"\\'..val..'"') end
 
-local _go_to_beginning = {'^', '0', to_char('<Home>')}
-local _go_to_end       = {'$', to_char('<End>')}
-local _move_left       = {'b', 'j', 'h', to_char('<Left>'), to_char('<PageUp>')}
-local _move_right      = {'w', 'k', 'l', to_char('<Right>'), to_char('<PageDown>')}
-local _shift_left      = {'B', 'J', 'H', to_char('<S-Left>'), to_char('<S-PageUp>')}
-local _shift_right     = {'W', 'K', 'L', to_char('<S-Right>'), to_char('<S-PageDown>')}
+local _go_to_beginning    = {'^', '0', to_char('<Home>'),   to_char('<Up>')}
+local _shift_to_beginning = {     ')', to_char('<S-Home>'), to_char('<S-Up>')}
+
+local _go_to_end    = {'$', to_char('<End>'),   to_char('<Down>')}
+local _shift_to_end = {'%', to_char('<S-End>'), to_char('<S-Down>')}
+
+local _go_to_left = {'b', 'j', 'h', to_char('<Left>'),   to_char('<PageUp>')}
+local _shift_left = {'B', 'J', 'H', to_char('<S-Left>'), to_char('<S-PageUp>')}
+
+local _go_to_right = {'w', 'k', 'l', to_char('<Right>'),   to_char('<PageDown>')}
+local _shift_right = {'W', 'K', 'L', to_char('<S-Right>'), to_char('<S-PageDown>')}
 
 --[[
 	/*
@@ -49,13 +54,20 @@ local function _modeInstruction()
 	-- If `getchar()` reported a number, convert it back to a character.
 	uinput = (type(uinput) == 'number') and string.char(uinput) or uinput
 
-	    if contains(_go_to_beginning, uinput) then nvim_command('tabfirst')
-	elseif contains(_go_to_end,       uinput) then nvim_command('tablast')
-	elseif contains(_move_left,       uinput) then nvim_command('tabprevious')
-	elseif contains(_shift_left,      uinput) then nvim_command('-tabmove')
-	elseif contains(_move_right,      uinput) then nvim_command('tabnext')
-	elseif contains(_shift_right,     uinput) then nvim_command('+tabmove')
-	elseif _combos[uinput]                    then _combos[uinput]()
+	-- Determine what to do with the input.
+	    if contains(_go_to_beginning,    uinput) then nvim_command('tabfirst')
+	elseif contains(_shift_to_beginning, uinput) then nvim_command('0tabmove')
+
+	elseif contains(_go_to_end,          uinput) then nvim_command('tablast')
+	elseif contains(_shift_to_end,       uinput) then nvim_command('$tabmove')
+
+	elseif contains(_go_to_left,         uinput) then nvim_command('tabprevious')
+	elseif contains(_shift_left,         uinput) then nvim_command('-tabmove')
+
+	elseif contains(_go_to_right,        uinput) then nvim_command('tabnext')
+	elseif contains(_shift_right,        uinput) then nvim_command('+tabmove')
+
+	elseif _combos[uinput]                       then _combos[uinput]()
 	end
 end
 
