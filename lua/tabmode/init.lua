db6baf0a1454a4cae96ee0b320a9a908c6a63ef4
Iron-E <code.iron.e@gmail.com>
2022-04-19 12:56:50 -0400

    perf(tabmode): use more API functions

--- lua/tabmode/init.lua
+++ lua/tabmode/init.lua
@@ -1,106 +1,91 @@
---[[
-	/*
-	 * IMPORTS
-	 */
---]]
-
-local vim  = vim
-local cmd  = vim.api.nvim_command
-local eval = vim.api.nvim_eval
-local fn   = vim.api.nvim_call_function
-
---[[
-	/*
-	 * MODULE
-	 */
---]]
+--[[/* MODULE */]]
 
 -- Wrap some vim command in a function.
-local function _cmd_wrap(command)
-	return function() cmd(command) end
+local function cmd(command)
+	return function() vim.api.nvim_command(command) end
 end
 
 -- the key combos for this mode.
 local _combos = {
-	['$'] = _cmd_wrap('tablast'),
-	['%'] = _cmd_wrap('$tabmove'),
-	[')'] = _cmd_wrap('0tabmove'),
-	['0'] = _cmd_wrap('tabfirst'),
-	['?'] = _cmd_wrap('help tabmode-usage'),
-	['a'] = _cmd_wrap('tabnew'),
-	['A'] = _cmd_wrap('$tabnew'),
-	['b'] = _cmd_wrap('tabprevious'),
-	['B'] = _cmd_wrap('-tabmove'),
-	['d'] = _cmd_wrap('tabclose'),
-	['i'] = _cmd_wrap('-tabnew'),
-	['I'] = _cmd_wrap('0tabnew'),
-	['s'] = function()
-		fn('execute', {{'tabnew', 'tabprevious', 'tabclose'}})
-	end,
+	['$'] = cmd 'tablast',
+	['%'] = cmd '$tabmove',
+	[')'] = cmd '0tabmove',
+	['0'] = cmd 'tabfirst',
+	['?'] = cmd 'help tabmode-usage',
+	['a'] = cmd 'tabnew',
+	['A'] = cmd '$tabnew',
+	['b'] = cmd 'tabprevious',
+	['B'] = cmd '-tabmove',
+	['d'] = cmd 'tabclose',
+	['i'] = cmd '-tabnew',
+	['I'] = cmd '0tabnew',
+	['s'] = function() vim.fn.execute {'tabnew', 'tabprevious', 'tabclose'} end,
 	['t'] = function()
-		local dir = fn('input', {'Select New `tcd`: ', '', 'dir'})
-		cmd('tcd '..(dir ~= '' and dir or fn('getcwd', {})))
+		vim.ui.input(
+			{completion = 'dir', prompt = [[Select new `tcd`:]]},
+			function(dir)
+				if dir then
+					vim.api.nvim_command('tcd '.. dir)
+				end
+			end
+		)
 	end,
-	['w'] = _cmd_wrap('tabnext'),
-	['W'] = _cmd_wrap('+tabmove'),
+	['w'] = cmd('tabnext'),
+	['W'] = cmd('+tabmove'),
 }
 
 -- create a `new` link for some `existing` mapping
-local function _combo_link(new, existing)
+local function inherit(new, existing)
 	_combos[new] = _combos[existing]
 end
 
 -- Turn some special character value into a character code.
-local function _to_char(val)
-	return eval('"\\'..val..'"')
+local function char(val)
+	return vim.api.nvim_replace_termcodes(val, true, true, true)
 end
 
 -- Synonyms for '0'
-_combo_link('^', '0')
-_combo_link(_to_char('<Home>') , '0')
-_combo_link(_to_char('<Up>')   , '0')
+inherit('^', '0')
+inherit(char '<Home>' , '0')
+inherit(char '<Up>'   , '0')
 
 -- Synonyms for ')'
-_combo_link(_to_char('<S-Home>'), ')')
-_combo_link(_to_char('<S-Up>'), ')')
+inherit(char '<S-Home>', ')')
+inherit(char '<S-Up>', ')')
 
 -- Synonyms for '$'
-_combo_link(_to_char('<End>'), '$')
-_combo_link(_to_char('<Down>'), '$')
+inherit(char '<End>', '$')
+inherit(char '<Down>', '$')
 
 -- Synonyms for '%'
-_combo_link(_to_char('<S-End>'), '%')
-_combo_link(_to_char('<S-Down>'), '%')
+inherit(char '<S-End>', '%')
+inherit(char '<S-Down>', '%')
 
 -- Synonyms for 'b'
-_combo_link('j', 'b')
-_combo_link('h', 'b')
-_combo_link(_to_char('<Left>'), 'b')
-_combo_link(_to_char('<PageUp>'), 'b')
+inherit('j', 'b')
+inherit('h', 'b')
+inherit(char '<Left>', 'b')
+inherit(char '<PageUp>', 'b')
 
 -- Synonyms for 'B'
-_combo_link('J', 'B')
-_combo_link('H', 'B')
-_combo_link(_to_char('<S-Left>'), 'B')
-_combo_link(_to_char('<S-PageUp>'), 'B')
+inherit('J', 'B')
+inherit('H', 'B')
+inherit(char '<S-Left>', 'B')
+inherit(char '<S-PageUp>', 'B')
 
 -- Synonyms for 'w'
-_combo_link('k', 'w')
-_combo_link('l', 'w')
-_combo_link(_to_char('<Right>'), 'w')
-_combo_link(_to_char('<PageDown>'), 'w')
+inherit('k', 'w')
+inherit('l', 'w')
+inherit(char '<Right>', 'w')
+inherit(char '<PageDown>', 'w')
 
 -- Synonyms for 'W'
-_combo_link('K', 'W')
-_combo_link('L', 'W')
-_combo_link(_to_char('<S-Right>'), 'W')
-_combo_link(_to_char('<S-PageDown>'), 'W')
+inherit('K', 'W')
+inherit('L', 'W')
+inherit(char '<S-Right>', 'W')
+inherit(char '<S-PageDown>', 'W')
 
---[[
-	/*
-	 * PUBLICIZE MODULE
-	 */
---]]
+--[[/* PUBLICIZE MODULE */]]
 
 return function()
 	require('libmodal').mode.enter('TABS', _combos)
